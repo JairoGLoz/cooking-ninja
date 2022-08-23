e04e9de3bf5638e495861dbda2c1c5529dfd86c2
Jairo Lozano <jairo25492@gmail.com>
2022-08-22 22:04:34 -0600

    implement color theme selector

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
 import {Link} from "react-router-dom";
+import {useTheme} from "../hooks/useTheme";
 
 // styles
 import './Navbar.css'
+
+// components
 import SearchBar from "./SearchBar";
 
 
 export default function Navbar() {
+
+    const {color} = useTheme()
+
     return (
-        <div className="navbar">
+        <div className="navbar" style={{background:color}}>
             <nav>
                 <Link to="/" className="brand">
                     <h1>Cooking Ninja</h1>
